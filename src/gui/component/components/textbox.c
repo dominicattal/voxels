f9285c31d9fd5e0f4d2fd8e4bee151ea5dfb7440
da526 <da526@njit.edu>
2024-11-30 01:40:40 -0500

    identified bug, bounding box 1 pixel off of ascender. literally about to make my own ttf parser

--- src/gui/component/components/textbox.c
+++ src/gui/component/components/textbox.c
@@ -24,7 +24,7 @@ void comp_textbox_init(Component* comp)
     comp_set_valign(comp, ALIGN_TOP);
     comp_set_is_text(comp, TRUE);
     comp_set_font(comp, FONT_TWO);
-    comp_set_font_size(comp, 21);
+    comp_set_font_size(comp, 24);
     comp->data = data_create();
 }
 
